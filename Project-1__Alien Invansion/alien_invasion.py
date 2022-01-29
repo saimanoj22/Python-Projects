e16f53f6ce653bccc0f4d2a4ee3676ce6ae1fe95
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-01-29 15:52:56 +0530

    Add program to detect collisions between bullets and aliens

--- Project-1__Alien Invansion/alien_invasion.py
+++ Project-1__Alien Invansion/alien_invasion.py
@@ -75,6 +75,18 @@ class AlienInvansion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        '''Respond to bullet-alien collisions.'''
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _create_fleet(self):
         '''Create the fleet of aliens.'''
         # Create an alien and find the number of aliens in a row.
