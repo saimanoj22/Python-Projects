68bb882a68c005ca16f7fafa5006c2adab257d28
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-01-29 15:58:34 +0530

    Update program to detect alien-ship collisions

--- Project-1__Alien Invansion/alien_invasion.py
+++ Project-1__Alien Invansion/alien_invasion.py
@@ -134,6 +134,10 @@ class AlienInvansion:
         self._check_fleet_edges()
         self.aliens.update()
 
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
+
     def _update_screen(self):
         '''Update images on the screen, and flip to the new screen.'''
         self.screen.fill(self.settings.bg_color)
