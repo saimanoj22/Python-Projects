0414821523a5d1031cbe6541b7aacc3e914f0ffa
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-03-09 15:58:36 +0530

    Add level displaying functionality

--- Project-1__Alien Invansion/alien_invasion.py
+++ Project-1__Alien Invansion/alien_invasion.py
@@ -72,6 +72,7 @@ class AlienInvansion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -136,6 +137,10 @@ class AlienInvansion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _create_fleet(self):
         '''Create the fleet of aliens.'''
