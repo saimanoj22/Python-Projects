301f1d0de85fc8bff56fa54e631282e87a371a6a
Etcherla Sai Manoj <esmanoj.95@gmail.com>
2022-03-09 12:05:09 +0530

    Add Levelling up functionality

--- Project-1__Alien Invansion/alien_invasion.py
+++ Project-1__Alien Invansion/alien_invasion.py
@@ -62,6 +62,9 @@ class AlienInvansion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset game settings.
+            self.settings.initialize_dynamic_settings()
+            
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -122,6 +125,7 @@ class AlienInvansion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _create_fleet(self):
         '''Create the fleet of aliens.'''
